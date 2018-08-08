2ea7b3eb50493a5745dbf0aeec7b86a0ccabc50f
wecassidy <wec.notif.github@gmail.com>
2018-08-08 10:38:53 -0700

    Enable rotation, CW only. TODO: redraw on rotate

--- display_music.py
+++ display_music.py
@@ -1,3 +1,4 @@
+import enum, itertools, math
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gdk, Gtk, Poppler
@@ -7,6 +8,15 @@ REPEATS = [
     (4, 2),
     (3, 4)
 ]
+
+# View configuration constants
+class Viewer(enum.Enum):
+    NORMAL = enum.auto()
+    INVERTED = enum.auto()
+    LEFT = enum.auto()
+    RIGHT = enum.auto()
+
+ORIENTATION = itertools.cycle([Viewer.NORMAL, Viewer.RIGHT, Viewer.INVERTED, Viewer.LEFT])
 
 def resolve_repeats(repeat_sequence, num_pages):
     next_repeat = next(repeat_sequence)
@@ -27,6 +37,7 @@ class SMPlayer(Gtk.Window):
         # Load document
         self.document = Poppler.Document.new_from_file("file://{}".format(file), None)
         self.pages = [self.document.get_page(i) for i in range(self.document.get_n_pages())]
+        self.orientation = next(ORIENTATION)
 
         # Resolve repeats, if any
         if repeats is not None:
@@ -63,22 +74,38 @@ class SMPlayer(Gtk.Window):
     def keyboard_handler(self, widget, key):
         if key.keyval == Gdk.KEY_Left:
             self.to_prev_page()
+        elif key.keyval == Gdk.KEY_r:
+            self.orientation = next(ORIENTATION)
         else:
             self.to_next_page()
 
     def draw(self, widget, surface):
         current_page = self.pages[self.page_order[self.page_order_pos]]
-
-        # Calculate scaling
         page_size = current_page.get_size()
         widget_size = widget.get_allocated_size()[0]
 
-        horizontal_scale = widget_size.width / page_size[0]
-        vertical_scale = widget_size.height / page_size[1]
+        # Calculate scaling
+        if self.orientation == Viewer.NORMAL or self.orientation == Viewer.INVERTED: # Upright or upside down
+            horizontal_scale = widget_size.width / page_size[0]
+            vertical_scale = widget_size.height / page_size[1]
+        else: # On its side
+            horizontal_scale = widget_size.width / page_size[1]
+            vertical_scale = widget_size.height / page_size[0]
 
         scale = min(horizontal_scale, vertical_scale) # Use the lesser scale to avoid overflow
+        surface.scale(scale, scale) # Apply scaling
 
-        surface.scale(scale, scale)
+        # Change orientation
+        if self.orientation == Viewer.LEFT:
+            surface.rotate(-math.pi/2)
+            surface.translate(-page_size[0], 0)
+        elif self.orientation == Viewer.RIGHT:
+            surface.rotate(math.pi/2)
+            surface.translate(0, -page_size[1])
+        elif self.orientation == Viewer.INVERTED:
+            surface.rotate(math.pi)
+            surface.translate(-page_size[0], -page_size[1])
+
         current_page.render(surface)
 
     def update_page(self):
